d37e019c61aff0ec34dea799826e4adcb6e1a561
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2021-12-30 01:02:30 +0530

    test

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def botpage():
             print(e)
             return render_template('index.html')   
         return render_template('index.html')
-    recivedstring = somet.split(" ")[0]
+    recivedstring = somet.split(" ")[1]
     print(recivedstring)
     bytestr = recivedstring.encode("ascii")
     print(bytestr)
